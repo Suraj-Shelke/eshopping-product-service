4d8b34d0f5a896b537c177bfb83539af9aeac747
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-04 17:50:53 +0530

    initial setup

---
+++ src/main/java/com/sp/eshopping_product_service/EshoppingProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshoppingProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshoppingProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/eshopping_product_service/EshoppingProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
