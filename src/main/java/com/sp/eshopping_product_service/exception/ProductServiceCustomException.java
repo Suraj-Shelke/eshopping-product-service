acea3f3980827b1965b67a293dd50e20658a4efc
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-05 23:24:27 +0530

    fix

--- src/main/java/com/sp/eshopping_product_service/exception/ProductServiceCustomException.java
+++ src/main/java/com/sp/eshopping_product_service/exception/ProductServiceCustomException.java
@@ -8,4 +8,12 @@ public class ProductServiceCustomException extends RuntimeException{
         super(message);
         this.errorCode = errorCode;
     }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
 }
