d870c5a6a09139e2a9610a1162d9b6d6748be8cf
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-06 14:28:13 +0530

    implemented service layer logic

--- src/main/java/com/sp/eshopping_product_service/entitiy/Product.java
+++ src/main/java/com/sp/eshopping_product_service/entitiy/Product.java
@@ -17,8 +17,7 @@ public class Product {
     public Product() {
     }
 
-    public Product(long productId, String productName, long price, long quantity) {
-        this.productId = productId;
+    public Product(String productName, long price, long quantity) {
         this.productName = productName;
         this.price = price;
         this.quantity = quantity;
@@ -32,5 +31,37 @@ public class Product {
         sb.append(", quantity=").append(quantity);
         sb.append('}');
         return sb.toString();
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
     }
 }
--- src/main/java/com/sp/eshopping_product_service/service/impl/ProductServiceImpl.java
+++ src/main/java/com/sp/eshopping_product_service/service/impl/ProductServiceImpl.java
@@ -1,29 +1,65 @@
 package com.sp.eshopping_product_service.service.impl;
 
+import com.sp.eshopping_product_service.entitiy.Product;
+import com.sp.eshopping_product_service.exception.ProductServiceCustomException;
 import com.sp.eshopping_product_service.payload.request.ProductRequest;
 import com.sp.eshopping_product_service.payload.response.ProductResponse;
+import com.sp.eshopping_product_service.repository.ProductRepository;
 import com.sp.eshopping_product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public long addProduct(ProductRequest productRequest) {
-        return 0;
+        Product product=new Product(productRequest.getName(),productRequest.getPrice(),productRequest.getQuantity());
+        product= productRepository.save(product);
+        return product.getProductId();
     }
 
     @Override
     public ProductResponse getProductById(long productId) {
-        return null;
+
+        Product product=productRepository.findById(productId)
+                .orElseThrow(()->new ProductServiceCustomException("Product with given Id not found","PRODUCT_NOT_FOUND"));
+        ProductResponse productResponse=new ProductResponse(product.getProductName(),
+                product.getProductId(),product.getQuantity(),product.getPrice());
+        return productResponse;
     }
 
     @Override
     public void reduceQuantity(long productId, long quantity) {
 
+        Product product=productRepository.findById(productId)
+                .orElseThrow(()-> new ProductServiceCustomException("Product with given Id not found", "PRODUCT_NOT_FOUND"));
+
+        if(product.getQuantity()<quantity){
+            throw new ProductServiceCustomException(
+                    "Product does not have sufficient Quantity",
+                    "INSUFFICIENT_QUANTITY"
+            );
+        }
+
+        product.setQuantity(product.getQuantity()-quantity);
+        productRepository.save(product);
     }
 
     @Override
     public void deleteProductById(long productId) {
 
+        if(!productRepository.existsById(productId)){
+            throw new ProductServiceCustomException("Product with given with Id: " + productId + " not found:",
+                    "PRODUCT_NOT_FOUND");
+        }
+
+        productRepository.deleteById(productId);
     }
 }
