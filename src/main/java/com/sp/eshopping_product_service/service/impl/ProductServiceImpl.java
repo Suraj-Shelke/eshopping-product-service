26224c4220b98e0e34a4ac8c06d2d0323c232fb5
suraj-shelke <shelkesuraj205@gmail.com>
2025-12-06 18:20:01 +0530

    log implemented in service

--- src/main/java/com/sp/eshopping_product_service/service/impl/ProductServiceImpl.java
+++ src/main/java/com/sp/eshopping_product_service/service/impl/ProductServiceImpl.java
@@ -6,6 +6,8 @@ import com.sp.eshopping_product_service.payload.request.ProductRequest;
 import com.sp.eshopping_product_service.payload.response.ProductResponse;
 import com.sp.eshopping_product_service.repository.ProductRepository;
 import com.sp.eshopping_product_service.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private static final Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -20,24 +23,29 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public long addProduct(ProductRequest productRequest) {
+        log.info("ProductServiceImpl | addProduct is called");
         Product product=new Product(productRequest.getName(),productRequest.getPrice(),productRequest.getQuantity());
         product= productRepository.save(product);
+        log.info("ProductServiceImpl | addProduct | Product Created");
+        log.info("ProductServiceImpl | addProduct | Product Id : " + product.getProductId());
         return product.getProductId();
     }
 
     @Override
     public ProductResponse getProductById(long productId) {
-
+        log.info("ProductServiceImpl | getProductById is called");
+        log.info("ProductServiceImpl | getProductById | Get the product for productId: {}", productId);
         Product product=productRepository.findById(productId)
                 .orElseThrow(()->new ProductServiceCustomException("Product with given Id not found","PRODUCT_NOT_FOUND"));
         ProductResponse productResponse=new ProductResponse(product.getProductName(),
                 product.getProductId(),product.getQuantity(),product.getPrice());
+        log.info("ProductServiceImpl | getProductById | productResponse :" + productResponse.toString());
         return productResponse;
     }
 
     @Override
     public void reduceQuantity(long productId, long quantity) {
-
+        log.info("Reduce Quantity {} for Id: {}", quantity,productId);
         Product product=productRepository.findById(productId)
                 .orElseThrow(()-> new ProductServiceCustomException("Product with given Id not found", "PRODUCT_NOT_FOUND"));
 
@@ -50,16 +58,17 @@ public class ProductServiceImpl implements ProductService {
 
         product.setQuantity(product.getQuantity()-quantity);
         productRepository.save(product);
+        log.info("Product Quantity updated Successfully");
     }
 
     @Override
     public void deleteProductById(long productId) {
-
+        log.info("Product id: {}", productId);
         if(!productRepository.existsById(productId)){
             throw new ProductServiceCustomException("Product with given with Id: " + productId + " not found:",
                     "PRODUCT_NOT_FOUND");
         }
-
+        log.info("Deleting Product with id: {}", productId);
         productRepository.deleteById(productId);
     }
 }
